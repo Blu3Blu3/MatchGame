7c9177de7ee7243c9790701be78604741b6e829d
Christian G <110692306+Blu3Blu3@users.noreply.github.com>
2025-09-03 17:05:50 -0700

    Added full interactivity only, no win/fail states

--- MatchGame/MainWindow.xaml.cs
+++ MatchGame/MainWindow.xaml.cs
@@ -8,6 +8,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+// This one gives you timers!
+using System.Windows.Threading;
 
 namespace MatchGame
 {
@@ -36,6 +38,7 @@ namespace MatchGame
                 "🐘","🐘"
             };
 
+            // You need to have a Random variable to act as the randomness seed before doing anything with System.Random's functions... er, methods, in C#.
             Random rng = new Random();
             foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
             {
@@ -43,6 +46,31 @@ namespace MatchGame
                 string nextEmoji = animalEmoji[index];
                 textBlock.Text = nextEmoji;
                 animalEmoji.RemoveAt(index);
+            }
+        }
+
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false;
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // The "as" operator casts something to another data type.
+            TextBlock textBlock = sender as TextBlock;
+            if (findingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+            }
+            else if (textBlock.Text == lastTextBlockClicked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+            }
+            else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                findingMatch = false;
             }
         }
     }
